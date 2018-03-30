3dd903870ec7b63e35ae822c2a3ed60baa1d1fd4
Xavier <xaviem01@NYUMC.ORG>
2018-03-30 10:28:28 -0400

    Initial Commit

---
+++ algorithms/kangaroo.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+def kangaroo(x1, v1, x2, v2):
+    """
+    Given the starting positions and velocities of two "kangaroos", determine
+    whether the kangaroos will meet at the same ending position within the
+    same number of jumps.
+
+    Args:
+        x1 <int>: the starting position of the first kangaroo
+        v1 <int>: the velocity of the first kangaroo
+        x1 <int>: the starting position of the second kangaroo
+        v2 <int>: the velocity of the second kangaroo
+
+    Return:
+        str: YES or NO
+    """
+    
+    return
+
+
+def main():
+    """
+    The main entry point for the script
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    kangaroo(0, 3, 4, 2)
+    
+    return
+    
+if __name__ == '__main__':
+    main()
